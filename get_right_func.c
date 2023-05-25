64ddae69e4596160640f8065872828d620dffe73
AbdessammeD <aitlhaj.abdessamed@gmail.com>
2023-05-25 11:17:20 -0700

    operations

--- get_right_func.c
+++ get_right_func.c
@@ -15,6 +15,11 @@ void (*get_right_func())(stack_t**, unsigned int)
 		{"pop", pop_stack},
 		{"swap", swap_stack},
 		{"add", add_stack},
+		{"nop", nop_skip},
+		{"sub", sub_stack},
+		{"div", div_stack},
+		{"mul", mul_stack},
+		{"mod", mod_stack},
 		{NULL, NULL}
 	};
 	int i = 0;
@@ -22,11 +27,22 @@ void (*get_right_func())(stack_t**, unsigned int)
 
 	strcpy(data_copy, data);
 	token = strtok(data_copy, " \t\n");
-	if (strcmp(token, "nop") == 0)
-		exit(EXIT_SUCCESS);
 	for (i = 0; instruction[i].opcode; i++)
 		if (strcmp(instruction[i].opcode, token) == 0)
 			return (instruction[i].f);
 
 	return (no_match);
+}
+
+/**
+ * nop_skip - skip the line.
+ * @stack: linked list.
+ * @line_number: the current line number.
+ *
+ * Return: nothing.
+ */
+void nop_skip(stack_t **stack, unsigned int line_number)
+{
+	(void) stack;
+	(void) line_number;
 }
---
+++ operations.c
@@ -0,0 +1,160 @@
+#include "monty.h"
+
+/**
+ * add_stack - adds of the top two elements of the stack.
+ * @stack: linked list.
+ * @line_number: the current line number.
+ *
+ * Return: nothing.
+ */
+void add_stack(stack_t **stack, unsigned int line_number)
+{
+	int len = 0;
+	stack_t *tmp = *stack;
+
+	while (tmp != NULL)
+	{
+		len++;
+		tmp = tmp->next;
+	}
+
+	if (len < 2)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	tmp = *stack;
+	tmp->next->n += tmp->n;
+	tmp->next->prev = NULL;
+	*stack = tmp->next;
+	free(tmp);
+}
+/**
+ * sub_stack -  sub of the top two elements of the stack.
+ * @stack: linked list.
+ * @line_number: the current line number.
+ *
+ * Return: nothing.
+ */
+void sub_stack(stack_t **stack, unsigned int line_number)
+{
+	int len = 0;
+	stack_t *tmp = *stack;
+
+	while (tmp != NULL)
+	{
+		len++;
+		tmp = tmp->next;
+	}
+
+	if (len < 2)
+	{
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	tmp = *stack;
+	tmp->n -= tmp->next->n;
+	tmp->next->prev = NULL;
+	*stack = tmp->next;
+	free(tmp);
+}
+
+/**
+ * mul_stack -  mul of the top two elements of the stack.
+ * @stack: linked list.
+ * @line_number: the current line number.
+ *
+ * Return: nothing.
+ */
+void mul_stack(stack_t **stack, unsigned int line_number)
+{
+	int len = 0;
+	stack_t *tmp = *stack;
+
+	while (tmp != NULL)
+	{
+		len++;
+		tmp = tmp->next;
+	}
+
+	if (len < 2)
+	{
+		fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	tmp = *stack;
+	tmp->next->n *= tmp->n;
+	tmp->next->prev = NULL;
+	*stack = tmp->next;
+	free(tmp);
+}
+
+/**
+ * div_stack -  div of the top two elements of the stack.
+ * @stack: linked list.
+ * @line_number: the current line number.
+ *
+ * Return: nothing.
+ */
+void div_stack(stack_t **stack, unsigned int line_number)
+{
+	int len = 0;
+	stack_t *tmp = *stack;
+
+	while (tmp != NULL)
+	{
+		len++;
+		tmp = tmp->next;
+	}
+
+	if (len < 2)
+	{
+		fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	tmp = *stack;
+	tmp->n /= tmp->next->n;
+	tmp->next->prev = NULL;
+	*stack = tmp->next;
+	free(tmp);
+}
+
+/**
+ * mod_stack -  mod of the top two elements of the stack.
+ * @stack: linked list.
+ * @line_number: the current line number.
+ *
+ * Return: nothing.
+ */
+void mod_stack(stack_t **stack, unsigned int line_number)
+{
+	int len = 0;
+	stack_t *tmp = *stack;
+
+	while (tmp != NULL)
+	{
+		len++;
+		tmp = tmp->next;
+	}
+
+	if (len < 2)
+	{
+		fprintf(stderr, "L%d: can't mod, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	tmp = *stack;
+	tmp->next->n %= tmp->n;
+	tmp->next->prev = NULL;
+	*stack = tmp->next;
+	free(tmp);
+}
